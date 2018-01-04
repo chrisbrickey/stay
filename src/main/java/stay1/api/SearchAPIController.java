6f4f3d7d9537ea606ef5da9b4f24abd142336e98
chrisbrickey <cbrickey1@gmail.com>
2018-01-04 13:00:06 -0800

    add API to controller for API to differentiate from frontend controller

--- src/main/java/stay1/api/SearchAPIController.java
+++ src/main/java/stay1/api/SearchAPIController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SearchController {
+public class SearchAPIController {
 
     private static final String template = "These are the results for %s:";
     private final AtomicLong counter = new AtomicLong();
